4dc3666a852a827cecd8893e1c0e158bcac35b59
Shilcof <robert.shilcof@gmail.com>
2020-10-27 15:57:58 +0000

    commit tree 68fba3297ef2e646d6cceb13136f4d815a5cec42

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,24 @@
 class Cat
   # code goes here
+
+  @@all = []
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    self.owner = owner
+    self.mood = mood
+    save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+  
 end
